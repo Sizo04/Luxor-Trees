45a6858bf2440aef78805d1a59b145fb098a0c8e
Sizo04 <sizowork@gmail.com>
2023-05-10 18:31:21 +0200

    responsive nav

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuBtn = document.querySelector('.button')
+const dropdownMenu = document.querySelector('.dropdown-menu')
+
+
+menuBtn.onclick = function(){
+    dropdownMenu.classList.toggle('open')
+
+   
+}
