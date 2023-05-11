ed1be3dbf63cb684fd8443059aa5e2067a25785c
Sizo04 <sizowork@gmail.com>
2023-05-11 17:43:29 +0200

    animation

--- script.js
+++ script.js
@@ -2,18 +2,27 @@ const menuBtn = document.querySelector('.button')
 const dropdownMenu = document.querySelector('.dropdown-menu')
 let dropLinks = document.querySelector('.drop-close');
 
-
-
-
 menuBtn.onclick = function(){
     dropdownMenu.classList.toggle('open')
 
     dropLinks.onclick = function(){
         dropdownMenu.classList.remove('open')
     }
-
-    
    
 }
 
 
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }
+        else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+
+const hiddenElements = document.querySelectorAll('.offer')
+hiddenElements.forEach((el) => observer.observe(el))
