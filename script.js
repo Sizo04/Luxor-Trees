8c3f093961297d53a54b770021743f5b4b29229b
Sizo04 <sizowork@gmail.com>
2023-05-11 13:15:54 +0200

    nav fixes

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const menuBtn = document.querySelector('.button')
 const dropdownMenu = document.querySelector('.dropdown-menu')
+let dropLinks = document.querySelector('.drop-close');
+
+
 
 
 menuBtn.onclick = function(){
     dropdownMenu.classList.toggle('open')
 
+    dropLinks.onclick = function(){
+        dropdownMenu.classList.remove('open')
+    }
+
+    
    
 }
+
+
