2de4c5f82ab0ba3de4e6b0ec8e265a50192bb7ff
Sizo04 <sizowork@gmail.com>
2023-07-15 18:52:49 +0200

    add to cart done

--- script.js
+++ script.js
@@ -29,10 +29,37 @@ hiddenElements.forEach((el) => observer.observe(el))
 
 //buy
 function Buy(element){
-let mainEl = element.closest(".text");
+let mainEl = element.closest(".plant-container");
  let price = mainEl.querySelector(".price").innerText;
+ let name = mainEl.querySelector(".name").innerText;
+ let image = mainEl.querySelector(".image").getAttribute('src');
+ let cartItem = document.querySelector(".item");
  price = price.substring(1);
 price = parseInt(price);
- console.log(price);
+console.log(image);
+cartItem.innerHTML += `
+<div class="item-info">
+<img id="cart-image" src="${image}" alt="" />
+<h2 id="cart-name">${name}</h2>
+<h2 id="cart-price">${price}</h2>
+</div>  `
 
 }
+
+let cartToggle = document.querySelector("#cartToggle");
+let cartMenu = document.querySelector(".cart-list");
+
+
+let i = 0;
+cartToggle.addEventListener("click", () => {
+    if(i <= 0){
+        cartMenu.style.display = "block";
+        i++
+    }else{
+        cartMenu.style.display = "none";
+       i = 0;
+    }
+    
+})
+
+
